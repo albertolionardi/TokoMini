74d2476866e3550b477615883f3681418cebf3cd
Alberto <albertolionardi1@gmail.com>
2025-02-05 16:25:53 +0100

    implement route and ontroller for admin

--- api/controllers/adminController.js
+++ api/controllers/adminController.js
@@ -9,9 +9,8 @@ const adminController = {
       const result = await client.query("SELECT * FROM admins WHERE email = $1", [email]);
       const admin = result.rows[0];
       if (!admin) {
-        return res.status(401).json({ message: "Invalid Credentials" });
+        return res.status(401).json({ message: "Invalid credentials" });
       }
-      console.log("Admin Logged in:", admin);
       if (admin.password !== password) {
         return res.status(401).json({ message: "Invalid credentials" });
       }
@@ -23,9 +22,144 @@ const adminController = {
       });
     } catch (err) {
       console.log("Error in login:", err);
-      next(err);
+      return res.status(500).json({ message: "Internal server error" });
+
     }
   },
+  createProduct : async (req,res,next) => {
+    const {productName,productPrice,productCategory,productImg,productStock,productDescription} = req.body;
+    if (!productName || !productPrice || !productCategory || !productImg || !productStock || !productDescription) {
+      return res.status(400).json({ message: "All fields are required!" });
+    }
+    try{
+      const result = await client.query(
+        `INSERT INTO products 
+        (productname, productprice, productcategory, productimg, productstock, productdescription) 
+        VALUES ($1, $2, $3, $4, $5, $6) RETURNING *`, 
+        [productName, productPrice, productCategory, productImg, productStock, productDescription])
+      const insertedProduct = result.rows[0]
+      return res.status(201).json({
+        message: "Product created successfully!",
+        product: {
+          productName: insertedProduct.productname,
+          productPrice: insertedProduct.productprice,
+          productCategory: insertedProduct.productcategory,
+          productImg: insertedProduct.productimg,
+          productStock: insertedProduct.productstock,
+          productDescription: insertedProduct.productdescription
+        }
+      });
+    }
+    catch(err){
+      console.log("Error creating product")
+      return res.status(500).json({ message: "Internal server error" });
+
+    }
+  },
+  readProducts : async(req,res,next) => {
+    const page = parseInt(req.query.page) || 1
+    const limit = parseInt(req.query.limit) || 10;
+    const offset = (page - 1) * limit;
+
+    try {
+      const result = await client.query(`SELECT * FROM products LIMIT $1 OFFSET $2`, [limit, offset]);
+      const products = result.rows;
+      if (products.length === 0) {
+        return res.status(404).json({ message: "No products found" });
+      }
+      return res.json({
+        page: page,
+        limit: limit,
+        total: products.length, 
+        products: products.map(product => ({
+          productId: product.productid,
+          productName: product.productname,
+          productPrice: product.productprice,
+          productCategory: product.productcategory,
+          productImg: product.productimg,
+          productStock: product.productstock,
+          productDescription: product.productdescription
+        }))
+      });
+    }
+    catch(err){
+      console.log("Error in fetching products:", err);
+      return res.status(500).json({ message: "Internal server error" });
+    }
+  },
+  updateProduct : async (req,res,next) => {
+    const productId = req.params.productId;
+    if (isNaN(productId) || productId <= 0) {
+      return res.status(400).json({ message: "Invalid product ID" });
+    }    
+    const {productName,productPrice,productCategory,productImg,productStock,productDescription} = req.body;
+    if (!productName || !productPrice || !productCategory || !productImg || !productStock || !productDescription) {
+      return res.status(400).json({ message: "All fields are required!" });
+    }
+    try{
+      const result = await client.query(
+        `UPDATE products 
+        SET productname = $1, productprice = $2, productcategory = $3, productimg = $4, productstock = $5, productdescription = $6 
+        WHERE productid = $7 
+        RETURNING *`, 
+        [productName, productPrice, productCategory, productImg, productStock, productDescription, productId])
+      const insertedProduct = result.rows[0]
+      return res.status(201).json({
+        message: "Product updated successfully!",
+        product: {
+          productName: insertedProduct.productname,
+          productPrice: insertedProduct.productprice,
+          productCategory: insertedProduct.productcategory,
+          productImg: insertedProduct.productimg,
+          productStock: insertedProduct.productstock,
+          productDescription: insertedProduct.productdescription
+        }
+      });
+    }
+    catch(err){
+      console.log("Error updating product")
+      return res.status(500).json({ message: "Internal server error" });
+    }
+
+  },
+  deleteProduct: async (req, res, next) => {
+    const productId = req.params.productId;
+    if (isNaN(productId) || productId <= 0) {
+      return res.status(400).json({ message: "Invalid product ID" });
+    }
+    try {
+      const product = await client.query("SELECT * FROM products WHERE productid = $1", [productId]);
+      if (product.rows.length === 0) {
+        return res.status(404).json({ message: "Product not found" });
+      }
+      await client.query("DELETE FROM products WHERE productid = $1", [productId]);
+      return res.status(200).json({ message: "Product deleted successfully" });
+    } catch (err) {
+      console.log("Error deleting product", err);
+      return res.status(500).json({ message: "Internal server error" });
+    }
+  },
+
+  readUsers: async(req,res,next) => {
+    try {
+      const users = await client.query("SELECT userid, name, email FROM users");
+      if (users.rows.length == 0) {
+        return res.status(404).json({ message: "No users available" });
+      }
+      return res.status(200).json({
+        message: "Users retrieved successfully",
+        users: users.rows
+      });
+
+    }
+    catch(err) {
+      console.error("Error reading users:", err);
+      return res.status(500).json({ message: "Internal server error" });
+    }
+  },
+
+  
+
 };
 
 module.exports = adminController;
--- api/routes/adminRoutes.js
+++ api/routes/adminRoutes.js
@@ -4,5 +4,13 @@ const router = express.Router();
 const adminController = require('../controllers/adminController');  
 
 router.post('/login', adminController.login);
+router.post('/createproduct', adminController.createProduct);
+router.get('/viewProducts', adminController.readProducts);
+router.put('/updateProduct/:productId', adminController.updateProduct);
+router.delete('/deleteProduct/:productId', adminController.deleteProduct);
+router.get('/getUsers', adminController.readUsers)
+
+
+
 
 module.exports = router;  

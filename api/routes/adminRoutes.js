61e8003f72022ba809f83224450e2b233f2efc87
Alberto <albertolionardi1@gmail.com>
2025-02-06 08:33:31 +0100

    routes now use their specifics middleware

--- api/routes/adminRoutes.js
+++ api/routes/adminRoutes.js
@@ -2,14 +2,14 @@
 const express = require("express");
 const router = express.Router();
 const adminController = require('../controllers/adminController');  
-const authMiddleware = require('../../middleware/authMiddleware');
+const adminAuthMiddleware = require('../../middleware/adminAuthMiddleware');
 
 router.post('/login', adminController.login);
-router.post('/createproduct', authMiddleware, adminController.createProduct);
-router.get('/viewProducts', authMiddleware, adminController.readProducts);
-router.put('/updateProduct/:productId', authMiddleware, adminController.updateProduct);
-router.delete('/deleteProduct/:productId', authMiddleware, adminController.deleteProduct);
-router.get('/getUsers', authMiddleware, adminController.readUsers)
+router.post('/createproduct', adminAuthMiddleware, adminController.createProduct);
+router.get('/viewProducts', adminAuthMiddleware, adminController.readProducts);
+router.put('/updateProduct/:productId', adminAuthMiddleware, adminController.updateProduct);
+router.delete('/deleteProduct/:productId', adminAuthMiddleware, adminController.deleteProduct);
+router.get('/getUsers', adminAuthMiddleware, adminController.readUsers)
 
 
 
--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -2,14 +2,14 @@
 const express = require("express");
 const router = express.Router();
 const userController = require('../controllers/userController');  
-const authMiddleware = require('../../middleware/authMiddleware');
+const userAuthMiddleware = require('../../middleware/userAuthMiddleware');
 
 router.post('/register', userController.register);
 router.post('/login', userController.login);
-router.get('/listProduct',authMiddleware, userController.listProduct);
-router.get('/viewProduct/:productId',authMiddleware, userController.getProductByid);
-router.post('/addToCart',authMiddleware, userController.addToCart);
-router.post('/order',authMiddleware, userController.submitOrder)
+router.get('/listProduct',userAuthMiddleware, userController.listProduct);
+router.get('/viewProduct/:productId',userAuthMiddleware, userController.getProductByid);
+router.post('/addToCart',userAuthMiddleware, userController.addToCart);
+router.post('/order',userAuthMiddleware, userController.submitOrder)
 
 
 

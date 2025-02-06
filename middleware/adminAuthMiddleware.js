239894f6d0f5f97a8ac012d1dac2191461adfe29
Alberto <albertolionardi1@gmail.com>
2025-02-06 08:30:39 +0100

    split middleware into adminmiddleware and usermiddleware to prevent admin accessing user's api and the other way around

--- middleware/adminAuthMiddleware.js
+++ middleware/adminAuthMiddleware.js
@@ -1,21 +1,23 @@
 const client = require('../dbClient');
 const jwt = require('jsonwebtoken');
 
-const authMiddleware = async (req,res,next) => {
-    const token = req.header('Authorization')?.replace('Bearer ', '')
+const adminAuthMiddleware = async (req,res,next) => {
+    const token = req.header('Authorization')?.replace('Bearer ', '');
     if (!token) {
         return res.status(401).json({ message: "Access denied. No token provided." });
       }
     try {
         const decoded = jwt.verify(token, process.env.secret);
-        const result = await client.query("SELECT * FROM admins WHERE adminid = $1 AND token = $2", [decoded.id, token]);
+        if (decoded.role !== 'admin') {
+            return res.status(403).json({ message: "Access denied. Admins only." });
+        }
+        const query = "SELECT * FROM admins WHERE adminid = $1 AND token = $2";
+        const result = await client.query(query, [decoded.id, token]);
         const admin = result.rows[0];
 
         if (!admin) {
             return res.status(401).json({ message: "Invalid token." });
         }
-
-        req.admin = admin;
         next();
     } catch (err) {
         console.log("Error in token validation:", err);
@@ -23,4 +25,4 @@ const authMiddleware = async (req,res,next) => {
     }
 }
 
-module.exports = authMiddleware
+module.exports = adminAuthMiddleware
---
+++ middleware/userAuthMiddleware.js
@@ -0,0 +1,29 @@
+const client = require('../dbClient');
+const jwt = require('jsonwebtoken');
+
+const userAuthMiddleware = async (req,res,next) => {
+    const token = req.header('Authorization')?.replace('Bearer ', '');
+    if (!token) {
+        return res.status(401).json({ message: "Access denied. No token provided." });
+      }
+      try {
+        const decoded = jwt.verify(token, process.env.secret);
+        if (decoded.role !== 'user') {
+            return res.status(403).json({ message: "Access denied. Users only." });
+        }
+        const query = "SELECT * FROM users WHERE userid = $1 AND token = $2";
+        const result = await client.query(query, [decoded.id, token]);
+        const user = result.rows[0];
+
+        if (!user) {
+            return res.status(401).json({ message: "Invalid token." });
+        }
+
+        req.user = user;
+        next();
+    } catch (err) {
+        console.error("Error in user token validation:", err);
+        return res.status(401).json({ message: "Invalid token." });
+    }
+}
+module.exports = userAuthMiddleware
